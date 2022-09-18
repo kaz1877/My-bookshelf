ebae0a02fd4820797c36a279d458dd9d19f742f7
github kaz1877 <k689208@gmail.com>
2022-09-18 19:54:09 +0900

    直接登録の処理を追加しました。

--- source/resources/views/api.blade.php
+++ source/resources/views/api.blade.php
@@ -13,36 +13,36 @@
 @else (count($items) > 0)
     <p>「{{ $keyword }}」の検索結果</p>
     @foreach ($items as $item)
-        <div class="listgroup">
-            <div class="list_title">
-                <h2 class="h3 mb-2 mt-2 pd-4">{{ $item['volumeInfo']['title']}}</h2>
-            </div>
-            <div class="list_image">
-                @if (array_key_exists('imageLinks', $item['volumeInfo']))
-                    <img src="{{ $item['volumeInfo']['imageLinks']['smallThumbnail']}}">
-                @endif
-            </div>
-            <div class="list_contents">
-                <div class="list_details">
-                    @if (array_key_exists('description', $item['volumeInfo']))
-                        <p>著者：{{ $item['volumeInfo']['authors'][0]}}</p>
-                    @endif
-                    @if (array_key_exists('description', $item['volumeInfo']))
-                        <p>発売年月：{{ $item['volumeInfo']['publishedDate']}}</p>
-                    @endif
-                    <!-- @if (array_key_exists('description', $item['volumeInfo']))
-                        概要：{{ $item['volumeInfo']['description']}}<br>
-                    @endif -->
-                </div>
-                <form action="/api" method="POST">
-                        @csrf
+        <form action="/api" method="POST">
+        @csrf
+            <div class="listgroup">
+                <div class="list_title">
+                    <h2 class="h3 mb-2 mt-2 pd-4">{{ $item['volumeInfo']['title']}}</h2>
                     <input type="hidden" name="title" value="{{ $item['volumeInfo']['title']}}">
-                    <input type="hidden" name="author" value="{{ $item['volumeInfo']['authors'][0]}}">
-                    <input type="hidden" name="url" value="{{ $item['volumeInfo']['imageLinks']['smallThumbnail']}}">
+                </div>
+                <div class="list_image">
+                    @if (array_key_exists('imageLinks', $item['volumeInfo']))
+                        <img src="{{ $item['volumeInfo']['imageLinks']['smallThumbnail']}}">
+                        <input type="hidden" name="url" value="{{ $item['volumeInfo']['imageLinks']['smallThumbnail']}}">
+                    @endif
+                </div>
+                <div class="list_contents">
+                    <div class="list_details">
+                        @if (array_key_exists('authors', $item['volumeInfo']))
+                            <p>著者：{{ $item['volumeInfo']['authors'][0]}}</p>
+                            <input type="hidden" name="author" value="{{ $item['volumeInfo']['authors'][0]}}">
+                        @endif
+                        @if (array_key_exists('publishedDate', $item['volumeInfo']))
+                            <p>発売年月：{{ $item['volumeInfo']['publishedDate']}}</p>
+                        @endif
+                        <!-- @if (array_key_exists('description', $item['volumeInfo']))
+                            概要：{{ $item['volumeInfo']['description']}}<br>
+                        @endif -->
+                    </div>
                     <div class="text-right">
                         <button type="submit" class="btn btn-primary">本棚に追加する。</button>
                     </div>
-                </form>
+        </form>
             </div>
         </div>
     @endforeach
