fb5491462a4b2395788f84b1041cd25437bec93b
kaz1877 <k689208@gmail.com>
2023-12-25 01:04:29 +0900

    laravel9へアップグレード、vue,vueRouter,vuexの導入

---
+++ source/resources/js/store/index.js
@@ -0,0 +1,12 @@
+import Vuex from 'vuex'
+
+const createStore = () => {
+    return new Vuex.Store({
+        state: function() {
+            return {
+                message: 'Hello Vuex!'
+            }
+        }
+    })
+}
+export default createStore
